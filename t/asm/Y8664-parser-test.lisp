a28fbdb7c759d537b56dc523aaae94000219b903
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-01-10 12:15:02 -0500

    init parser test file

--- t/asm/Y8664-parser-test.lisp
+++ t/asm/Y8664-parser-test.lisp
@@ -1,6 +1,9 @@
+;;;; Testing for the definitions in src/asm/Y8664-parser.lisp
+
 (defpackage Y8664-parser-test
   (:export #:run-tests)
   (:nicknames #:parser-t)
   (:use #:cl #:fiveam))
 
 (in-package #:Y8664-parser-test)
+
