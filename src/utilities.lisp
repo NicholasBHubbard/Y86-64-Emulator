1481c0c72f8bccad2fa3bf3202b5afa5b68fda8a
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-02-12 16:59:07 -0500

    Add personal utilities

---
+++ src/utilities.lisp
@@ -0,0 +1,36 @@
+(defpackage utilities
+  (:use #:cl)
+  (:nicknames #:utils #:u)
+  (:local-nicknames (#:a #:alexandria))
+  (:export #:const
+           #:make-keyword
+           #:defstruct-read-only))
+
+(in-package #:utilities)
+
+;;; ===============================================
+
+(defun const (x)
+  "Return a function that takes one argument and always returns X."
+  (lambda (y) (declare (ignore y)) x))
+
+;;; ===============================================
+
+(defun make-keyword (name)
+  "Interns the string designated by NAME in the KEYWORD package."
+  (intern (string-upcase (string name)) :keyword))
+
+;;; ===============================================
+
+(defmacro defstruct-read-only (name &body options)
+  "Like DEFSTRUCT but each field is made read-only."
+  (flet ((append-read-only (options)
+           (loop :for opt :in options
+                 :collect (append (a:remove-from-plist opt :read-only)
+                                  (list :read-only t)))))
+    (if (stringp (first options))
+        `(defstruct ,name
+           ,(first options)
+           ,@(append-read-only (rest options)))
+        `(defstruct ,name
+           ,@(append-read-only options)))))
