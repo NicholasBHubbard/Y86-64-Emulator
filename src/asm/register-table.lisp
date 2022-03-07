354130c0f91a2f314634a6b56b34e72f0f7e84ac
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-03-06 22:44:57 -0500

    Implement REGISTER-TABLE with U:DEFCLOSURE.

--- src/asm/register-table.lisp
+++ src/asm/register-table.lisp
@@ -5,7 +5,7 @@
 (defpackage register-table
   (:use #:cl)
   (:local-nicknames (#:a #:alexandria))
-  (:export #:*register-table*
+  (:export #:register-table
            #:register))
 
 (in-package #:register-table)
@@ -22,8 +22,48 @@
 
 ;;; ==================== Register Table Definition ====================
 
-(defun init-register-table ()
-  "Initialize the *REGISTER-TABLE* global closure variable."
+(u:defclosure register-table
+  "Lexical closure over the Y86-64 static register table. This closure uses
+LOL:DLAMBDA to provide various function specifiers for dynamically querying the
+register table. The following documents the various function specifiers and
+their argument lists.
+
+:id-p id
+  Return T if ID is a valid Y86-64 register id and return NIL otherwise.
+
+:register-name-p reg
+  Return T if REG is a valid Y86-64 register and return NIL otherwise. REG can
+  be either a keyword or string.
+
+:all-ids
+  Return a list of all the Y86-64 register ids.
+
+:all-register-names
+  Return a list of all the Y86-64 register name keywords.
+
+:all-id-strings
+  Return a list of all the Y86-64 register id's as strings.
+
+:all-register-name-strings
+  Return a list of all the Y86-64 register names as strings.
+
+:id-register-name id
+  Return the register name keyword with id ID.
+
+:register-name-id reg
+  Return the id of the register named REG. REG can be either a keyword or
+  string.
+
+:id-register-name-string id
+  Return the register name string of the register with id ID.
+
+:register-name-id-string reg
+  Return the id of the register named REG as a string. REG can be either a
+  keyword or string.
+
+:id-register-name-match-p id reg
+  Return T if the register REG has id ID and return NIL otherwise. REG can be
+  either a keyword or string. "
   (let ((register-table
           (list
            (make-entry :id #x0 :name :RAX)
@@ -42,85 +82,60 @@
            (make-entry :id #xD :name :R13)
            (make-entry :id #xE :name :R14)
            (make-entry :id #xF :name :NOREG))))
-    (lambda (function-keyword &rest inputs)
-      ;; turn input strings into keywords so user the can choose input format
-      (let ((inputs (mapcar (lambda (input)
-                              (if (stringp input)
-                                  (u:make-keyword input)
-                                  input))
-                            inputs)))
-        (case function-keyword
-          (:id-p
-           ;; T iff (FIRST INPUTS) is a valid Y86-64 register id
-           (if (member (first inputs) (mapcar #'entry-id register-table) :test #'=)
-               t))
-          
-          (:register-name-p
-           ;; T iff (FIRST INPUTS) is a valid Y86-64 register name
-           (if (member (first inputs) (mapcar #'entry-name register-table))
-               t))
-          
-          (:all-ids
-           ;; list of all the Y86-64 register ids
-           (sort (mapcar #'entry-id register-table) #'<))
-          
-          (:all-register-names
-           ;; list of all the Y86-64 register names
-           (sort (mapcar #'entry-name register-table) #'string<))
-          
-          (:all-id-strings
-           ;; like :ALL-IDS except stringify the ids
-           (sort (mapcar
-                  (a:compose (a:curry #'format nil "~x") #'entry-id)
-                  register-table)
-                 #'string<))
-          
-          (:all-register-name-strings
-           ;; like :ALL-REGISTER-NAMES except stringify the register names
-           (sort (mapcar (a:compose #'symbol-name #'entry-name) register-table)
-                 #'string<))
-          
-          (:id-register-name
-           ;; the register name associated to the id (FIRST INPUTS)
-           (entry-name
-            (find-if
-             (a:compose (a:curry #'= (first inputs)) #'entry-id)
-             register-table)))
-          
-          (:register-name-id
-           ;; the id of the register named (FIRST INPUTS)
-           (entry-id
-            (find-if
-             (a:compose (a:curry #'eql (first inputs)) #'entry-name)
-             register-table)))
-          
-          (:id-register-name-string
-           ;; like :ID-REGISTER-NAME except stringify the register name
-           (symbol-name
-            (entry-name
-             (find-if
-              (a:compose (a:curry #'= (first inputs)) #'entry-id)
-              register-table))))
-          
-          (:register-name-id-string
-           ;; like :REGISTER-NAME-ID except stringify the id
-           (format nil "~x"
-                   (entry-id
-                    (find-if
-                     (a:compose (a:curry #'eql (first inputs)) #'entry-name)
-                     register-table))))
+    (lol:dlambda
+     (:id-p (id)
+            (if (member (first inputs) (mapcar #'entry-id register-table) :test #'=)
+                t))
+     
+     (:register-name-p (reg)
+                       (if (member (u:as-keyword reg) (mapcar #'entry-name register-table))
+                           t))
+     
+     (:all-ids ()
+               (sort (mapcar #'entry-id register-table) #'<))
+     
+     (:all-register-names ()
+                          (sort (mapcar #'entry-name register-table) #'string<))
+     
+     (:all-id-strings ()
+                      (sort (mapcar
+                             (a:compose (a:curry #'format nil "~x") #'entry-id)
+                             register-table)
+                            #'string<))
+     
+     (:all-register-name-strings ()
+                                 (sort (mapcar (a:compose #'symbol-name #'entry-name) register-table)
+                                       #'string<))
+     
+     (:id-register-name (id)
+                        (entry-name
+                         (find-if
+                          (a:compose (a:curry #'= id) #'entry-id)
+                          register-table)))
+     
+     (:register-name-id (reg)
+                        (entry-id
+                         (find-if
+                          (a:compose (a:curry #'eql (u:as-keyword reg)) #'entry-name)
+                          register-table)))
+     
+     (:id-register-name-string (id)
+                               (symbol-name
+                                (entry-name
+                                 (find-if
+                                  (a:compose (a:curry #'= id) #'entry-id)
+                                  register-table))))
+     
+     (:register-name-id-string (reg)
+                               (format nil "~x"
+                                       (entry-id
+                                        (find-if
+                                         (a:compose (a:curry #'eql (u:as-keyword reg)) #'entry-name)
+                                         register-table))))
 
-          (:id-register-name-match-p
-           ;; T iff the id (FIRST INPUTS) has register name (SECOND INPUTS)
-           (eql (second inputs)
-                (entry-name
-                 (find-if
-                  (a:compose (a:curry #'= (first inputs)) #'entry-id)
-                  register-table))))
-
-          (otherwise
-           (error 'internal (format nil "The symbol ~a does not denote a valid *REGISTER-TABLE* function" function-keyword))))))))
-
-(defparameter *register-table* (init-register-table)
-  "Lexical closure over the static Y86-64 register table that can be used to
-query information about registers.")
+     (:id-register-name-match-p (id reg)
+                                (eql (u:as-keyword reg)
+                                     (entry-name
+                                      (find-if
+                                       (a:compose (a:curry #'= id) #'entry-id)
+                                       register-table)))))))
